7158b535019f48fe7c4885b3cc569e0516482ff8
brock elmore <brock.elmore@gmail.com>
2024-08-01 10:31:28 -0700

    table

--- src/lib/components/custom/table.svelte
+++ src/lib/components/custom/table.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div class="w-full flex items-center justify-center prose-limit">
-	<table class="not-prose rounded-none font-light not-italic text-gray-11 bg-gray-3">
+	<table class="not-prose prose-limit rounded-none font-light not-italic text-gray-11 bg-gray-3" style="transform: scale(0.85);">
 		<slot />
 	</table>
 </div>
