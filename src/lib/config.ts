1c461aba2798f5a849c1022c555e435b91b22bd1
brock elmore <brock.elmore@gmail.com>
2024-08-15 11:52:21 -0700

    fix categories

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,4 +4,4 @@ export const title = 'brocks blog'
 export const description = 'brocks personal blogx'
 export const url = dev ? 'http://localhost:5173/' : 'https://brocke.xyz/'
 
-export const categories = ['math', 'pyrometer', 'interval analysis']
+export const categories = ['math', 'pyrometer', 'interval analysis', 'solidity']
